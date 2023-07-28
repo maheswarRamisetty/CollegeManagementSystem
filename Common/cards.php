f1c135f8c7689429a8aca2b1dae353e1cf3fa42d
maheswarRamisetty <106472608+maheswarRamisetty@users.noreply.github.com>
2023-07-28 21:18:36 +0530

    Add files via upload

---
+++ Common/cards.php
@@ -0,0 +1,45 @@
+</section>
+	<section class=" bg-light text-center">
+		<div class=" w-100 pt-5 m-auto" style="
+	width:100%;
+	padding-right:15px;
+	padding-left:15px;
+	margin-right:auto;
+	margin-left:auto;
+">
+			<h1>Departments</h1>
+			<div class="row pt-3 pb-3">
+				<div class="col-lg-3 col-md-3 col-sm-12 col-10 m-auto">
+					<div class="card card-margin">
+						<div class="card-logo m-auto">
+							<img src="images/1.jpg" class="card-img img-fluid">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Civil and Mechanical Engineering</h5>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-lg-3 col-md-3 col-sm-12 col-10 m-auto">
+					<div class="card card-margin">
+						<div class="card-logo m-auto">
+							<img src="images/3.jpg" class="card-img img-fluid">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Information Technology</h5>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-12 col-10 m-auto">
+					<div class="card card-margin">
+						<div class="card-logo m-auto">
+							<img src="images/4.jpg" class="card-img img-fluid">
+						</div>
+						<div class="card-body">
+							<h5 class="card-title">Computer Science</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</section>
---
+++ Common/footer.php
@@ -0,0 +1,38 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>VVIT COLLEGE</h3>
+            <p>This Project has been made by Maheshwar.
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About VVIT</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.TECH Civil Engineering</a></li>
+              <li><a href="">B.TECH Electrical Engineering</a></li>
+              <li><a href="">B.TECH Mechanical Engineering</a></li>
+              <li><a href="">B.TECH Computer Science</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address:NAMBUR</p>
+              <p>Telephones: (+91 8328679737)</p>
+              <p>E-mail: maheshwarramisetty9@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by Maheshwar</p>
+</footer>
